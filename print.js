7dea40f1c9e0d6655a1275b18aaae7f3d9b37eaa
Bastian Krol <bastian.krol@web.de>
2014-08-11 07:38:20 +0200

    fetch from Toggl in 24h chunks

--- print.js
+++ print.js
@@ -3,14 +3,39 @@
 require('./lib/init_nconf');
 
 var level = require('level')
-  , db = level('./data/worklog')
+  , dbToggl = level('./data/toggl')
+  , dbWorklog = level('./data/worklog')
   ;
 
-db.createReadStream({ valueEncoding: 'json' })
-.on('data', function(data) {
-  console.log(data.key + ':');
-  console.log(data.value);
-})
-.on('error', function(err) {
-  console.log('read error', err)
-});
+
+function printToggl() {
+  console.log('>>>> printing Toggl items');
+  printDb(dbToggl, printWorklog);
+}
+
+function printWorklog() {
+  console.log('>>>> printing worklog items');
+  printDb(dbWorklog, done);
+}
+
+function done() {
+  console.log('>>>> done');
+}
+
+function printDb(db, next) {
+  db.createReadStream({ valueEncoding: 'json' })
+  .on('data', function(data) {
+    console.log(data.key + ':');
+    console.log(data.value);
+  })
+  .on('error', function(err) {
+    console.log('read error', err)
+  })
+  .on('end', function() {
+    if (typeof next === 'function') {
+      next();
+    }
+  });
+}
+
+printToggl();
